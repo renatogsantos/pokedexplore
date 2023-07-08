813841ce5bec1967afee121db96e127ce71ebb79
RenatoGSantos <39703265+renatogsantos@users.noreply.github.com>
2023-07-07 21:34:41 -0300

    feat: adiciona head para a página

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,14 +3,18 @@ import "./globals.scss";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-export const metadata = {
-  title: "PokedExplore",
-  description: "Eu escolho você!",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="keywords"
+          content="PokedExplorer, Pokémon, exploration, game, creatures"
+        />
+        <title>PokédExplorer</title>
+      </head>
       <Provider store={store}>
         <body suppressHydrationWarning={true}>{children}</body>
       </Provider>
