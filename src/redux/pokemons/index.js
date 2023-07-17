f1624aaa3e491e15f57c3b9a08680f0f9b258d14
RenatoGSantos <39703265+renatogsantos@users.noreply.github.com>
2023-07-17 08:36:32 -0300

    refactor: alterando quantidade getPokemonToPokedex

--- src/redux/pokemons/index.js
+++ src/redux/pokemons/index.js
@@ -51,7 +51,7 @@ export const getPokemon = (pokemon) => {
 
 export const getPokemonToPokedex = () => {
   return async (dispatch) => {
-    const number = gerarNumeroAleatorio(150);
+    const number = gerarNumeroAleatorio(500);
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${number}`)
       .then((resp) => {
